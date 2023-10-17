5e20b1d389469678d507ea5112ba912f43c15386
zlJoseph <omar41_41@hotmail.com>
2023-10-17 09:41:32 -0500

    v1 Agregando logger Middleware y GCP

--- app/Middleware/AuthMicroservice.ts
+++ app/Middleware/AuthMicroservice.ts
@@ -3,9 +3,10 @@ import Env from '@ioc:Adonis/Core/Env'
 import { unauthorized } from 'Config/display'
 
 export default class AuthMicroservice {
-  public async handle({ request, response }: HttpContextContract, next: () => Promise<void>) {   
+  public async handle({ request, response, logger }: HttpContextContract, next: () => Promise<void>) {   
 
     if(request.header('X-APP-Key','')!==Env.get('APP_KEY')){
+      logger.warn({warn: unauthorized,headers: request.headers()}, 'Request MiddlewareServices')
       response.unauthorized({ error: unauthorized })
       return
     }
